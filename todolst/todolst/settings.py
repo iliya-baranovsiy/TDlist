3d6d8dc9cbdda618f2e7651157288f3edffad6f6
iliya-baranovsiy <mr.Fredi2017@mail.ru>
2025-02-07 20:04:28 +0300

    last

--- todolst/todolst/settings.py
+++ todolst/todolst/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ms%n-@+r=f4iz9cui-3h+o^gsf@fzgb^zuo-#xc!6fw6&&(sj3'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
